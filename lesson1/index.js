c2200dbbd1d996b47e421faed18a72270be14aa4
katerina <rrina1586@gmail.com>
2021-12-05 20:57:29 +0300

    урок 1

---
+++ lesson1/index.js
@@ -0,0 +1,7 @@
+// console.log(process.argv);
+// const [name] = process.argv.slice(2);
+// console.log(name);
+// console.log(`Hello, Node.js for ${ name }!`);
+
+const colors = require('colors');
+console.log(colors.magenta("Hello, Node.js"));
